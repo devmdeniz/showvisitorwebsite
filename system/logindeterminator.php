eea9b58bc076bfdab86280dc8211b7f22ce7707c
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-13 14:07:15 +0300

    Update logindeterminator.php

--- system/logindeterminator.php
+++ system/logindeterminator.php
@@ -4,7 +4,6 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 
 if($username == "mrdenizlp" && $password == "123456") {
-	$_SESSION["loginsuccessful"] = "successful";
 	header("Location: ../admin/login.php");
 } else {
 	header("Location: ../index.php");
