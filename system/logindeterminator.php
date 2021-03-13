2b8e53f09d183fe0ac894b0228c27ddb0e13cef0
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-13 14:03:25 +0300

    Fix some bugs

--- system/logindeterminator.php
+++ system/logindeterminator.php
@@ -5,8 +5,9 @@ $password = $_POST["password"];
 
 if($username == "mrdenizlp" && $password == "123456") {
 	$_SESSION["loginsuccessful"] = "successful";
+	header("Location: ../admin/login.php")
 } else {
-	header("Location ../index.php");
+	header("Location: ../index.php");
 }
 
 
