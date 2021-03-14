48215d2b38438bfd17d23b2e7cf963f74b956e2b
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-14 18:12:24 +0300

    Update update.php

--- admin/sections/update.php
+++ admin/sections/update.php
@@ -1,1 +1,12 @@
-<?php?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<form action="system/update.php" method="post">
+      <input pattern="\d*" type="text" placeholder="Enter the Visitor" name="updatenumber" required>
+      <button type="submit">Save Visitors</button>
+    </form>
+<?php
+?>
