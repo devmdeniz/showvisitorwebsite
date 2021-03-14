efca0196709beef8517ea6b52ed4db22912d0cb4
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-14 18:13:09 +0300

    Update create.php

--- admin/sections/create.php
+++ admin/sections/create.php
@@ -1,1 +1,12 @@
-<?php?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<form action="system/insert.php" method="post">
+      <input pattern="\d*" type="text" placeholder="Enter the Visitor" name="visitornumber" required>
+      <button type="submit">Create Visitors</button>
+    </form>
+<?php
+?>
