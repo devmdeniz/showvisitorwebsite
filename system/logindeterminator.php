d21adaa98e8474d66b1215c2938369457d01e809
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-14 13:04:42 +0300

    Update logindeterminator.php

--- system/logindeterminator.php
+++ system/logindeterminator.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 $username = $_POST["username"];
 $password = $_POST["password"];
@@ -9,5 +10,16 @@ if($username == "mrdenizlp" && $password == "123456") {
 	header("Location: ../index.php");
 }
 
+$determinator = $_POST["determinator"];
+
+	if($determinator == "update") {
+		$_SESSION["determinator"] = "update";
+	} else if($determinator == "create") {
+		$_SESSION["determinator"] = "create";
+	} else if ($determinator == "delete") {
+		$_SESSION["determinator"] = "delete";
+	} else {
+		$_SESSION["determinator"] = "dashboard";
+	}
 
 ?>
