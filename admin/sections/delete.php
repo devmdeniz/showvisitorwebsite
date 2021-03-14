88ae69ae05f52b41f44410c240e61cc3cc405f48
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-14 18:10:16 +0300

    Update delete.php

--- admin/sections/delete.php
+++ admin/sections/delete.php
@@ -1,3 +1,9 @@
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 <form action="../system/delete.php" method="post">
       <input pattern="\d*" type="text" placeholder="Delete the Visitor" name="deletenumber" required>
       <button type="submit">Delete Visitors</button>
