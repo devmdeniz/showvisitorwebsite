d90ea78132da537b8af23ec0f7f9102d219b616d
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-03-22 12:19:20 +0300

    Update update.php

--- admin/sections/update.php
+++ admin/sections/update.php
@@ -4,7 +4,7 @@
 <br>
 <br>
 <br>
-<form action="system/update.php" method="post">
+<form action="../system/update.php" method="post">
       <input pattern="\d*" type="text" placeholder="Enter the Visitor" name="updatenumber" required>
       <button type="submit">Save Visitors</button>
     </form>
