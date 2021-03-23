f38385a3857a6668cf88758231bd487828c314bf
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-03-23 14:59:38 +0300

    Update dashboard.php

--- admin/sections/dashboard.php
+++ admin/sections/dashboard.php
@@ -1,1 +1,16 @@
-<?php?>
+<?php
+include("../system/connection.php");
+
+$lastvisitornumber = $conn -> query("SELECT * FROM visitors ORDER BY visitornumber DESC LIMIT 1");
+$outputs = $lastvisitornumber->fetch_array();
+
+
+
+?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+    <p><?php echo $outputs[1];?></p>
